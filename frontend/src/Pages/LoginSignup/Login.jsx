3697dfd83d2eda28ca2b229c305d8b7f5cb6df79
sivavarunan <158249443+sivavarunan@users.noreply.github.com>
2024-03-03 12:45:38 +0530

    validation

--- frontend/src/Pages/LoginSignup/Login.jsx
+++ frontend/src/Pages/LoginSignup/Login.jsx
@@ -3,28 +3,54 @@ import { GiPadlock } from "react-icons/gi";
 import { MdEmail } from "react-icons/md";
 import Divider from "./Divider";
 import api from "../../api/api"; // Import the api.js file
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = ({ onSignupClick }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const getTokenFromLocalStorage = () => {
+    return localStorage.getItem('token');
+  };
+
 
   const handleLogin = async (e) => {
     e.preventDefault(); // Prevent the form from submitting automatically
 
-    // Perform login authentication here, you can use API calls similar to the signup component
-    try {
+    try {     
       console.log(email, password);
+
+      if (!email || !password) {
+        toast.error("All fields are required");
+        return;
+      }
+
       const responseData = await api.login(email, password);
-      console.log(responseData); // Output the response data
+      // console.log(responseData); // Output the response data
+      console.log(responseData?.message); // Output the response data
+      if(responseData?.message !== `Request failed with status code 401`){
+        localStorage.setItem('token', responseData.token); // Save token to local storage
+        toast.success(`Login Success!!`)
+        navigate('/');
+      }
+
       // Optionally, you can redirect the user to the login page upon successful signup
-      onLoginClick(); 
+      // onLoginClick(); 
       // Assuming `onLoginClick` is a function passed from the parent component to switch to the login view
 
       // Clear the form fields after logging
       setEmail("");
       setPassword("");
+
     } catch (error) {
       console.error("Error logging up:", error);
+      if (error.response?.data?.message){
+        toast.error(error.response?.data?.message)
+      }
       // Handle signup error, display error message, etc.
     }
   };
@@ -36,20 +62,20 @@ const Login = ({ onSignupClick }) => {
         <input
           type="email"
           placeholder="Email"
-          required
+          // required
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-        />
+        /><label class ="xy">E-mail</label>
         <MdEmail className="icon" />
       </div>
       <div className="input-box">
         <input
           type="password"
           placeholder="Password"
-          required
+          // required
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        />
+        /><label class ="xy">Password</label>
         <GiPadlock className="icon" />
       </div>
       <div className="remember-forgot">
--- frontend/src/Pages/LoginSignup/Signup.jsx
+++ frontend/src/Pages/LoginSignup/Signup.jsx
@@ -4,44 +4,128 @@ import { GiPadlock } from "react-icons/gi";
 import { MdEmail } from "react-icons/md";
 import Divider from "./Divider";
 import api from "../../api/api"; // Import the api.js file
+import { AxiosError } from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { MdDangerous } from "react-icons/md";
+import PasswordInput from "./PasswordInput";
+
+
 
 const Signup = ({ onLoginClick }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
 
+  const [gmailError, setGmailError]  = useState(false);
+  const[usernameError, setUsernameError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  const handleChange = (event) => {
+    if (event && event.target && event.target.value) {
+      setPassword(event.target.value);
+    }
+  };
+
   const handleSignup = async (e) => {
     e.preventDefault(); // Prevent the form from submitting automatically
 
     try {
       console.log(username, email, password);
+
+      if (!username || !email || !password) {
+        toast.error("All fields are required");
+        return;
+      }
+
+      if (!validateEmail(email)) {
+        setEmailError(true);
+        toast.error("Invalid email address");
+        return;
+      }
+
+      if (password.length < 6) {
+        setPasswordError(true);
+        toast.error("Password must be at least 6 characters long");
+        return;
+      }
+
+
       const responseData = await api.signUp(username, email, password);
-      console.log(responseData); // Output the response data
+      console.log(responseData?.message); // Output the response data
+      if(responseData?.message !== `Request failed with status code 400`){
+        setGmailError(false)
+        setUsernameError(false)
+        onLoginClick();
+        toast.success(`Sign up Success!!`)
+        
+      // }else if(responseData?.message !== `Request failed with status code 401`){
+      //   setGmailError(true)
+      //   setUsernameError(true)
+      //   toast.dismiss()
+      //   toast.error(`Sign up Failed!!`)
+
+      }else{
+        setGmailError(true)
+        setUsernameError(true)
+        toast.dismiss()
+        toast.error(`Sign up Failed!!`)
+      }
+      
+
+
       // Optionally, you can redirect the user to the login page upon successful signup
-      onLoginClick(); // Assuming `onLoginClick` is a function passed from the parent component to switch to the login view
+      //  // Assuming `onLoginClick` is a function passed from the parent component to switch to the login view
+
     } catch (error) {
       console.error("Error signing up:", error);
+      if (error.response?.data?.message){
+        toast.error(error.response?.data?.message)
+      }
       // Handle signup error, display error message, etc.
     }
+
   };
 
+  const validateEmail = (email) => {
+    // Regular expression for email validation
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+    
+
+  };
+ 
   return (
     <form>
       <h1 className="text">Sign Up</h1>
       <div className="input-box">
         <input type="text" placeholder="Username" required onChange={e => setUsername(e.target.value)} />
+        <label class ="xy">Username</label>
         <FaUserCircle className="icon" />
       </div>
+      {usernameError && (
+      <div className="errorh"><MdDangerous className="iconx"/> Username alredy exists</div>)}
       <div className="input-box">
-        <input type="email" placeholder="Email" required  onChange={e => setEmail(e.target.value)} />
+        <input type="email" placeholder="Email" required onChange={e => setEmail(e.target.value)} />
+        <label class ="xy">E-mail</label>
         <MdEmail className="icon" />
       </div>
-      <div className="input-box">
-        <input type="password" placeholder="Password" required  onChange={e => setPassword(e.target.value)} />
+      {gmailError && <div className="errorh"><MdDangerous className="iconx" /> Email already exists</div>}
+      {/* {emailError && <div className="errorh"><MdDangerous className="iconx"/> Invalid email address</div>} */}
+      <div className="box">
+        {/* <input type="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} /> */}
+        
+        <PasswordInput placeholder="Password" onChange={setPassword} />
         <GiPadlock className="icon" />
+        {/* <label class ="xy">Password</label> */}
+        
       </div>
+      
+      {/* <PasswordStrength placeholder="Password" onChange={handleChange} /> */}
       <div>
         <button type="button" className="submit gray" onClick={handleSignup}>
+          
           Sign up
         </button>
         <Divider />
@@ -49,6 +133,7 @@ const Signup = ({ onLoginClick }) => {
           Log in
         </button>
       </div>
+
     </form>
   );
 };
